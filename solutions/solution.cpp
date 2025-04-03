d6cf503423323384a2ad7b6ede3efd1c910f406e
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-04-03 14:50:50 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,16 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
-
-
 
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        vector<int> result(n);/***nothing i am doing */
-        int left = 0, right = n - 1;
-        int index = n - 1;
+        vector<int> result(n);
+        int left = 0, right = n - 1, index = n - 1;
 
         while (left <= right) {
             int leftSquare = nums[left] * nums[left];
@@ -20,33 +18,10 @@ public:
                 result[index--] = leftSquare;
                 left++;
             } else {
-                result[index--] = rightSquare;//fsfsdafdddddd
+                result[index--] = rightSquare;
                 right--;
             }
-        }/*head to safe jpne*/
-
+        }
         return result;
-    }/*lets see now*/
-};
-
-
-void printVector(const vector<int>& v) {
-    cout << "[";
-    for (size_t i = 0; i < v.size(); i++) {
-        cout << v[i];
-        if (i < v.size() - 1) cout << ", ";
     }
-    cout << "]" << endl;
-}
-
-
-int main() {
-    Solution solution;
-    vector<int> nums = {-4, -1, 0, 3, 10};
-    vector<int> result = solution.sortedSquares(nums);
-
-    cout << "Sorted Squares: ";
-    printVector(result);
-
-    return 0;
-}
+};
