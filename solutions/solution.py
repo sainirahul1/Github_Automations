4887a302de66c2842d8d23a49fcea943a66f87d5
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-04-08 15:05:37 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -2,26 +2,8 @@ from typing import List
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        result = [0] * n  # Create an output array of the same size
-        left, right = 0, n - 1
-        index = n - 1  # Start filling from the end
-
-        while left <= right:
-            left_square = nums[left] ** 2
-            right_square = nums[right] ** 2
-
-            if left_square > right_square:
-                result[index] = left_square
-                left += 1
-            else:
-                result[index] = right_square
-                right -= 1
-
-            index -= 1  # Move to the next position
-
-        return result
-
+        
+       
 # Testing the function
 if __name__ == "__main__":
     solution = Solution()
