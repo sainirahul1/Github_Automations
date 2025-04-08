9f3eafd5f2333e2cc8985c3fdf5fc8cea3ec258c
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-04-08 15:01:37 +0530

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,3 @@
-def sorted_squares(nums):
-    left, right = 0, len(nums) - 1
-    result = [0] * len(nums)
-    pos = len(nums) - 1
-    
-    while left <= right:
-        if abs(nums[left]) > abs(nums[right]):
-            result[pos] = nums[left] ** 2
-            left += 1
-        else:
-            result[pos] = nums[right] ** 2
-            right -= 1
-        pos -= 1
-    
-    return result
-
 def run_test_cases():
     test_cases = [
         [-10, -3, -2, 0, 1, 5, 7, 12],
