097bd7f1ee37d2cf015c2feed46b5dce698ef3db
saini rahul <sainirahul4148@gmail.com>
2025-04-08 15:41:41 +0530

    java check

--- solutions/solution.java
+++ solutions/solution.java
@@ -1,6 +1,20 @@
 public class SortedSquares {
     public static int[] sortedSquares(int[] nums) {
-        // TODO: Implement your logic
-        return new int[0]; // Placeholder
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1, pos = n - 1;
+
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+            if (leftSq > rightSq) {
+                result[pos--] = leftSq;
+                left++;
+            } else {
+                result[pos--] = rightSq;
+                right--;
+            }
+        }
+         return result;
     }
 }
