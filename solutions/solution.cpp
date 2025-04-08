7abdbb34b655dcb53b250dfbd1358dd231ccaaba
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-04-08 15:05:03 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -1,30 +1,10 @@
 #include <iostream>
 #include <vector>
-
-
-
 using namespace std;
 
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n);
-        int left = 0, right = n - 1, index = n - 1;
-
-        while (left <= right) {
-            int leftSquare = nums[left] * nums[left];
-            int rightSquare = nums[right] * nums[right];
-
-            if (leftSquare > rightSquare) {
-                result[index--] = leftSquare;
-                left++;
-            } else {
-                result[index--] = rightSquare;
-                right--;
-            }
-        }
-        return result;
-    }
+        
 };
 
