49d369811da0e2b38029231405aaec2d311d8c9f
sainirahul1 <147933478+sainirahul1@users.noreply.github.com>
2025-04-08 14:50:30 +0530

    Update test.c

--- tests/test.c
+++ tests/test.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-void sortedSquares(int* nums, int size, int* result) {
-    int left = 0, right = size - 1, pos = size - 1;
-
-    while (left <= right) {
-        if (abs(nums[left]) > abs(nums[right])) {
-            result[pos] = nums[left] * nums[left];
-            left++;
-        } else {
-            result[pos] = nums[right] * nums[right];
-            right--;
-        }
-        pos--;
-    }
-}
-
 void runTestCases() {
     int testCases[5][10] = {
         {-10, -3, -2, 0, 1, 5, 7, 12}, // Test case 1
